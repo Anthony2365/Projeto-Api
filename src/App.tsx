997da39f0af12a7a439ee8f79f3fda05c2289dec
Anthony2365 <anthonyvinicius124@gmail.com>
2022-01-28 16:14:30 -0300

    Projeto API

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App: React.FC = () => {
+  return (
+    <>
+    <h1>Hello world!</h1>
+    <h2>Anthony Vinicius Groff</h2>
+    </>
+  );
+}
+
+export default App;
